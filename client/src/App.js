444e7fee614cf51367c60b1d72ad6c3c0d823555
PabloAza89 <juanpabloazambuyo@gmail.com>
2022-10-27 02:33:48 -0300

    h

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,37 @@
-import './App.css';
+import React, { useState } from 'react';
+import MainPage from './MainPage';
+import LandingPage from './LandingPage';
+import { useDispatch, useSelector , connect } from 'react-redux';
+import { saveName } from './actions';
 
-function App() {
+export default function App() {
+  // const [useHook, setUseHook] = useState(true);
+  // const [show, setShow] = useState(true);
+
+  /* const dispatch = useDispatch() */
+
+  function useEmployees() {
+    return useSelector((state) => state.name)
+  }
+ 
+
+  //const componentShown = useHook ? <MainPage /> : <LandingPage />;
+
   return (
-    <div>
-      <div className='welcome'>
-        <button className='welcomeButton'></button>
+    <>
+      <div>
+        {console.log(useEmployees())}
+        {/* <button onClick={() => setUseHook(prev => !prev)}>
+          {`Change to ${useHook ? 'Class' : 'Hook'}`}
+        </button>
+        <button onClick={() => setShow(prev => !prev)}>
+          {show ? 'Hide' : 'Show'}
+        </button> */}
+        {/* <button onClick={() => dispatch(saveName(true))}>XX</button> */}
       </div>
-      <div className="main">
-        <h1>Henry Food</h1>
-      </div>
-    </div>
-  );
+      {
+        useEmployees() ? <MainPage /> : <LandingPage />
+      }
+    </>
+  )
 }
-
-export default App;
---
+++ client/src/MainPage.jsx
@@ -0,0 +1,13 @@
+import './MainPage.css';
+
+function MainPage() {
+  return (
+    <div className='main'>
+      <div>
+        <h1 className="text">Henry Food</h1>
+      </div>
+    </div>
+  );
+}
+
+export default MainPage;
