862cd208397620a50f53032bb161a744a66733f2
PabloAza89 <juanpabloazambuyo@gmail.com>
2022-11-01 21:04:14 -0300

    h

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,7 +4,7 @@ const { Router } = require('express');
 const axios = require('axios');
 require('dotenv').config();
 const { API_KEY1 , API_KEY2 , API_KEY3 , API_KEY4 , API_KEY5 } = process.env;
-const API_KEY = API_KEY3;
+const API_KEY = API_KEY4;
 const NUMBER = 1;
 
 const { Recipes , Diets , Recipes_Diets , Op } = require('../db.js'); // ADDED
@@ -14,12 +14,6 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
-// GET /recipes?name="...":
-// Obtener un listado de las recetas que contengan la palabra 
-// ingresada como query parameter
-//Si no existe ninguna receta mostrar un mensaje adecuado
-
 
 // PERFECT SEARCHER !
 router.get('/recipes', async (req, res) => {
@@ -45,17 +39,20 @@ router.get('/recipes', async (req, res) => {
             })
 
             let dietsArray = searchDBRecipes.map(e => e.Diets).map(e => e.map(e => e.title))
-       /*      let modifiedDBObj = {
-                id: findByIDinDB.id,
-                title: findByIDinDB.title,
-                summary: findByIDinDB.summary,
-                healthScore: findByIDinDB.healthScore,
-                analyzedInstructions: findByIDinDB.analyzedInstructions,
-                database: findByIDinDB.database,
-                Diets: dietsArray
-            }      */
-            let qq = searchDBRecipes.map(e => e.Diets = dietsArray.map(e => e))
+            
+            let arrayForDB = []
 
+            dietsArray.map(e => {
+                arrayForDB.push({
+                    id: searchDBRecipes[dietsArray.indexOf(e)].id,
+                    title: searchDBRecipes[dietsArray.indexOf(e)].title,
+                    summary: searchDBRecipes[dietsArray.indexOf(e)].summary,
+                    healthScore: searchDBRecipes[dietsArray.indexOf(e)].healthScore,
+                    analyzedInstructions: searchDBRecipes[dietsArray.indexOf(e)].analyzedInstructions,
+                    database: searchDBRecipes[dietsArray.indexOf(e)].database,
+                    diets: e
+                })
+            })
            
             const apiRawData = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&number=${NUMBER}&addRecipeInformation=true`);
             const searchAPIRecipes = apiRawData.data.results.map(e => {
@@ -73,8 +70,8 @@ router.get('/recipes', async (req, res) => {
             })
             const apiFilteredResult = searchAPIRecipes.filter(e => e.title.toLowerCase().includes(title.toLowerCase()));
             if (searchDBRecipes[0] === undefined && apiFilteredResult[0] === undefined) return res.status(400).send('No hay recetasss disponibles')
-            return res.status(200).send(qq)
-            //return res.status(200).send(searchDBRecipes.concat(apiFilteredResult))
+           
+            return res.status(200).send(arrayForDB.concat(apiFilteredResult))
         }
         
         const apiRawData = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&number=${NUMBER}&addRecipeInformation=true`);
@@ -93,32 +90,43 @@ router.get('/recipes', async (req, res) => {
         })
 
         const allDBRecipes = await Recipes.findAll({
-            attributes: ['id' , 'title', 'summary', 'healthScore', 'analyzedInstructions']
+            //attributes: ['id' , 'title', 'summary', 'healthScore', 'analyzedInstructions']
+            include: [{
+                model: Diets,
+                attributes: ['title'],
+                through: {
+                  attributes: []
+                }
+            }]
         })
-        res.status(200).send(allDBRecipes.concat(allAPIRecipes))
+
+        let dietsArray = allDBRecipes.map(e => e.Diets).map(e => e.map(e => e.title))
+            
+        let arrayForDB = []
+
+        dietsArray.map(e => {
+            arrayForDB.push({
+                id: allDBRecipes[dietsArray.indexOf(e)].id,
+                title: allDBRecipes[dietsArray.indexOf(e)].title,
+                summary: allDBRecipes[dietsArray.indexOf(e)].summary,
+                healthScore: allDBRecipes[dietsArray.indexOf(e)].healthScore,
+                analyzedInstructions: allDBRecipes[dietsArray.indexOf(e)].analyzedInstructions,
+                database: allDBRecipes[dietsArray.indexOf(e)].database,
+                diets: e
+            })
+        })
+
+        res.status(200).send(arrayForDB.concat(allAPIRecipes))
     }
     catch (e) {
         res.status(400).send('No hay recetas disponibles...')      
     }
 });
 
-// GET /recipes/{idReceta}:
-// Obtener el detalle de una receta en particular
-// Debe traer solo los datos pedidos en la ruta de detalle de receta
-// Incluir los tipos de dieta asociados
-
 router.get('/recipes/:id', async (req, res) => {
     const { id } = req.params;
     var findByIDinDB;
- /*    let qq = {
-        'id': undefined,
-        'title': undefined,
-        'summary': undefined,
-        'healthScore': undefined,
-        'analyzedInstructions': undefined,
-        'database': undefined,
-        'Diets': undefined
-    } */
+ 
     try {
         if (true) {
             const apiRawData = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&number=${NUMBER}&addRecipeInformation=true`);
@@ -156,7 +164,7 @@ router.get('/recipes/:id', async (req, res) => {
                     healthScore: findByIDinDB.healthScore,
                     analyzedInstructions: findByIDinDB.analyzedInstructions,
                     database: findByIDinDB.database,
-                    Diets: dietsArray
+                    diets: dietsArray
                 }
                 return res.status(200).send(modifiedDBObj)
 
@@ -170,30 +178,12 @@ router.get('/recipes/:id', async (req, res) => {
     }
 });
 
-// POST /recipes:
-// Recibe los datos recolectados desde el formulario controlado de la ruta 
-// de creación de recetas por body
-// Crea una receta en la base de datos relacionada con sus tipos de dietas.
-
-/* router.post('/recipes', async (req, res) => {
-    
-    const { title , summary , healthScore , instructions } = req.body;
-    try {
-        if ( title && summary && healthScore && instructions ) {
-            const recipes = await Recipes.create(req.body);
-            return res.status(200).json(recipes);
-        }
-    } catch (e) {
-        return res.status(404).send("Falta enviar algun dato");
-    }
-}); */
-
 router.post('/recipes', async (req, res) => {
     let title = ['Whole30', 'Pescetarian']
     // WORKING
     try {
         const createRecipe = await Recipes.create({
-            title: "fake title 0",
+            title: "fake rice",
             summary: "test summary",
             healthScore: 10,
             analyzedInstructions: 'these are the instructions'
@@ -210,69 +200,12 @@ router.post('/recipes', async (req, res) => {
         res.status(200).send(createRecipe)
 
     }
-
-    /* let title = ['Vegan', 'Primal']
-    // WORKING
-    try {
-        const createRecipe = await Recipes.create({
-            title: "fake title 0",
-            summary: "test summary",
-            healthScore: 10,
-            analyzedInstructions: 'these are the instructions'
-          });
-        const relatedDiets = await Diets.findAll({           
-            where: {
-                [Op.or]: [
-                    { title: title }                   
-                  ]
-            }
-
-        })
-        createRecipe.addDiets(relatedDiets)
-        res.status(200).send(createRecipe)
-
-    } */
-
    
-  /*     
-      const result = await User.findOne({
-        where: { username: 'p4dm3' },
-        include: Profile
-      }); */
-
-    /*// WORKING
-    try {
-        res.json(await Recipes.bulkCreate([
-            {   title: "fake title 1",
-                summary: "test summary",
-                healthScore: 10,
-                analyzedInstructions: 'these are the instructions'
-            },
-            {   title: "fake title 2",
-                summary: "test summary",
-                healthScore: 11,
-                analyzedInstructions: 'these are the instructions'
-            },
-            {   title: "fake title 3",
-                summary: "test summary",
-                healthScore: 12,
-                analyzedInstructions: 'these are the instructions'
-            }
-
-          ]))//.then(() => console.log("se cargo la fake recipe")));
-        
-    } */
     
     catch(e) {
         res.status(400).send("hubo un error en la precarga de datos")
     }
 });
-
-// GET /diets:
-// Obtener todos los tipos de dieta posibles
-// En una primera instancia, cuando no exista ninguno, 
-// deberán precargar la base de datos con los tipos de datos 
-// indicados por spoonacular acá
 
 // ONLY FOR FIRST MANUAL POST
 router.post('/diets', async (req, res) => {
@@ -289,7 +222,6 @@ router.post('/diets', async (req, res) => {
             { title: "Primal" },
             { title: "Low FODMAP" },
             { title: "Whole30" }
-            /* { title: ["Gluten Free", "Ketogenic", "Vegetarian" , "Lacto-Vegetarian", "Ovo-Vegetarian", "Vegan", "Pescetarian", "Paleo", "Primal", "Low FODMAP", "Whole30"]} */
           ]))//.then(() => console.log("Users data have been saved")));
     }
     catch(e) {
